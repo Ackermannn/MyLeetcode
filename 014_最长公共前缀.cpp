ff508b8cd8950f3a181058ea3c6150099cddcc96
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2019-10-10 13:30:45 +0800

    Update and rename 014_最长公共前缀 to 014_最长公共前缀.cpp

--- 014_最长公共前缀.cpp
+++ 014_最长公共前缀.cpp
@@ -6,13 +6,13 @@ public:
 	string longestCommonPrefix(vector<string>& strs) {
 		
 		if (strs.empty()) return "";
-		string out = strs[0];
+		string out = strs[0];				//初始化 第一个字符串为最终结果
 		for (int j = 1; j < strs.size(); j++) {
-			if (strs[j].size() < out.size()) out = out.substr(0, strs[j].size());
+			// if (strs[j].size() < out.size()) out = out.substr(0, strs[j].size()); // 没有这句更简洁
 			for (int i = 0; i < out.size(); i++) {
-				if (out[i] != strs[j][i])
+				if (out[i] != strs[j][i])      // 查询接下来的对应字符是否相等
 				{
-					out = out.substr(0, i);
+					out = out.substr(0, i);   // 不等的话，缩短结果
 					break;
 				}
 			}
