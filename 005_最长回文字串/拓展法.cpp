fbffceb112f1008dd370b5f603e247b1552f4806
Ackermannn <Ackermannn@users.noreply.github.com>
2019-10-24 12:57:39 +0800

    Create 拓展法.cpp

---
+++ 005_最长回文字串/拓展法.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		// add #
+		string extend = "#";
+		for (auto x : s) {
+			extend.push_back(x);
+			extend.append("#");
+		}
+	
+		string longest;
+		string::iterator iter_left, iter_right;
+		for (auto iter = extend.begin(); iter != extend.end(); iter++)
+		{
+			iter_left = iter;
+			iter_right = iter;
+			while (*iter_left == *iter_right)
+			{
+				if (iter_right - iter_left + 1 > longest.size()) {
+					longest.assign(iter_left, iter_right + 1);
+					
+				}
+				if (iter_left == extend.begin() or iter_right == extend.end() - 1) break;
+				iter_left -= 1;
+				iter_right += 1;
+			}
+		}
+		
+		// 去除#
+		string ans;
+		for (int i = 0; i < longest.size(); i++) {
+			if (longest[i] != '#') {
+				ans.push_back(longest[i]);
+			}
+		}
+		return ans;
+	}
+};
+
+int main() {
+	Solution solu;
+	cout << "result: " <<solu.longestPalindrome("") << endl;
+
+	return 0;
+}
