7258a271ea67d0697a4ae8b0e8e36bdfacd875de
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2019-11-04 13:09:13 +0800

    Create code.cpp

---
+++ 136_只出现一次的数字/code.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using std::vector;
+using std::cout;
+using std::endl;
+using std::map;
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		map<int,int> m;
+		for (int x : nums) {
+			if (m.find(x) == m.end()) m[x] = 1;
+			else if (m[x] == 1) m[x] = 2;
+		}
+		for (auto x : m) {
+			if (x.second == 1) return x.first;
+			
+		}
+		
+	}
+};
+int main() {
+	Solution solu;
+	vector<int> nums = { 2,2,1 };
+	cout << solu.singleNumber(nums) << endl;
+	return 0;
+
+}
