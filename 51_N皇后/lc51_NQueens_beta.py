e2966587600666333e017c6aa9864e0904b70f6b
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2019-11-12 23:56:31 +0800

    加说明

--- 51_N皇后/lc51_NQueens_beta.py
+++ 51_N皇后/lc51_NQueens_beta.py
@@ -1,3 +1,21 @@
+'''
+用数组X = [x1, x2, ..., xi, ..., xn]保存皇后i的位置坐标(i,xi) 即i行xi列是皇后的坐标
+
+对 X 进行全排列,列出所有全排列的可能,
+
+当 X 有 xi = xj + j - i 或者 xi = xj + i - j 情况排除它
+
+    因为皇后(i,xi),(j,xj)在对角线时,必在 y+x=b 或 y-x=b 的直线上
+
+    x1+y1=b=x2+y2 or x1-y1=b=x2-y2
+
+    (i,xi) 带入 (x1,y1) ,(j, xj) 带入(x2,y2)
+
+    得i + xi = j + xj or i - xi = j - xj
+    
+最后剩下的情况是本问题的解
+
+'''
 import itertools
 
 def insert(p,n):
