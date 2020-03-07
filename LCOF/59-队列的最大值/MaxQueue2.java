aad0b7147fab598d676143e4e230c088cbbe848b
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-03-07 23:37:10 +0800

    Create MaxQueue2.java

---
+++ LCOF/59-队列的最大值/MaxQueue2.java
@@ -0,0 +1,48 @@
+/*
+ 使用两个int[] 分别当 deque 与 queue
+*/
+class MaxQueue {
+    private int MAX = 10000;
+    private int[] deque;
+    private int[] queue;
+    private int front;
+    private int back;
+    private int de_back;
+    private int de_front;
+
+    public MaxQueue() {
+        queue = new int[MAX];
+        deque = new int[MAX];
+        front = 0;
+        back = 1;
+        de_back = 1;
+        de_front = 0;
+    }
+
+    public int max_value() {
+        if (back - front != 1)
+            return deque[de_front];
+        else
+            return -1;
+    }
+
+    public void push_back(int value) {
+        while (de_back > de_front && deque[de_back - 1] < value) {
+            --de_back;
+        }
+        deque[de_back] = value;
+        ++de_back;
+        queue[back - 1] = value;
+        ++back;
+    }
+
+    public int pop_front() {
+        if (back - front != 1) {
+            int temp = queue[front];
+            ++front;
+            if(temp == deque[de_front]) de_front++;
+            return temp;
+        } else
+            return -1;
+    }
+}
