0336de051c2c6efd9d4ab707ce1739c075e5ba53
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-04-04 11:59:29 +0800

    Create 176第二高的薪水.sql

---
+++ Algorithms/Easy/176_第二高的薪水/176第二高的薪水.sql
@@ -0,0 +1,10 @@
+# 题目地址：
+# https://leetcode-cn.com/problems/second-highest-salary/
+# Write your MySQL query statement below
+
+SELECT MAX(Salary) SecondHighestSalary
+FROM Employee
+WHERE Salary <> (
+	SELECT MAX(Salary)
+	FROM Employee
+);
