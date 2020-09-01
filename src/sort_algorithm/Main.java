3b8dcffdfd0395dff06181bc157451cf61d33f22
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-09-01 09:22:45 +0800

    插入排序

--- src/sort_algorithm/Main.java
+++ src/sort_algorithm/Main.java
@@ -2,13 +2,15 @@ package sort_algorithm;
 
 
 import sort_algorithm.simple_selection_sort.SelectSort;
+import sort_algorithm.straight_insert_sort.InsertSort;
 
 public class Main {
     public static void main(String[] args) {
 
-        Sort s = new SelectSort();
+        Sort s = new InsertSort();
 
-        int[] nums = {9, 1, 5, 8, 3, 7, 4, 6, 2};
+//        int[] nums = {9, 1, 5, 8, 3, 7, 4, 6, 2};
+        int[] nums = {9};
         s.sort(nums);
         for (int x : nums) {
             System.out.printf("%d ", x);
