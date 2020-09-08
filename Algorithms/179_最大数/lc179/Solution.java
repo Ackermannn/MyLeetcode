3fb0582b63b1765544b8f77d8ea193d4141d3803
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-09-08 09:47:27 +0800

    179

--- Algorithms/179_最大数/lc179/Solution.java
+++ Algorithms/179_最大数/lc179/Solution.java
@@ -7,17 +7,17 @@ interface Solution {
     String largestNumber(int[] nums);
 }
 
-class StringCompare implements Comparator<String> {
-
-    @Override
-    public int compare(String o1, String o2) {
-        String order1 = o1 + o2;
-        String order2 = o2 + o1;
-        return order2.compareTo(order1);
-    }
-}
 
 class Solution1 implements Solution {
+    static class StringCompare implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            String order1 = o1 + o2;
+            String order2 = o2 + o1;
+            return order2.compareTo(order1);
+        }
+    }
 
     @Override
     public String largestNumber(int[] nums) {
