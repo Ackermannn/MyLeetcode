a4780a6a3e8517cd63769cddbbcbf8f01f6b2cae
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-09-18 21:30:12 +0800

    HJ3

---
+++ 华为机试/HJ3/Main.java
@@ -0,0 +1,35 @@
+package HJ3;
+/**
+ * 牛客的华为机试环境要自己写输入输出
+ *
+ * 题目地址 https://www.nowcoder.com/ta/huawei 题号HJ3
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Solution {
+    public void solve(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums.length - 1 && nums[i] == nums[i + 1]) continue;
+            else
+                System.out.println(nums[i]);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            int len = scan.nextInt();
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++) {
+                arr[i] = scan.nextInt();
+            }
+            sol.solve(arr);
+        }
+    }
+}
