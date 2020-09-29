721bc59999d4f105a1191c186d289e41c6e00c6a
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-09-29 15:01:16 +0800

    lcof05

---
+++ LCOF/05-替换空格/lcof05/Solution.java
@@ -0,0 +1,21 @@
+package lcof05;
+
+/**
+ * 学习 StringBuilder
+ */
+public class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(c);
+
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+}
