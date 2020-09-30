5b33ac0455c84d69e1f6af254e2bf6f8eb924188
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-09-30 20:57:14 +0800

    lcof17

---
+++ LCOF/17-打印从1到最大的n位数/lcof17/Solution.java
@@ -0,0 +1,26 @@
+package lcof17;
+/*
+* 看评论区，此题在剑指offer 上难度很高
+*
+* */
+class Solution {
+    public int[] printNumbers(int n) {
+        int count = (int) Math.pow(10, n) - 1;
+        int[] ans = new int[count];
+        for (int i = 0; i < count; i++) {
+            ans[i] = i + 1;
+        }
+        return ans;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] ret = solution.printNumbers(3);
+        for (int x : ret) {
+            System.out.printf("%d,", x);
+        }
+    }
+
+}
