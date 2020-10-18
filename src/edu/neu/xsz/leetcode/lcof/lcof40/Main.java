56b476b1e858dc2b4344aa588ad04df46bba822c
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-10-18 21:07:46 +0800

    lcof40 优先队列

--- src/edu/neu/xsz/leetcode/lcof/lcof40/Main.java
+++ src/edu/neu/xsz/leetcode/lcof/lcof40/Main.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-//        Solution2 solution = new Solution2();
+//        Solution solution = new Solution();
+
+        Solution2 solution = new Solution2();
+
+        System.out.println("[INFO] Start run solution of Offer40...");
 
         int[] arr = {3, 2, 1};
         int k = 2;
 
         int[] ret = solution.getLeastNumbers(arr, k);
 
+        System.out.println("[INFO] Answer is: ");
         System.out.println(Arrays.toString(ret));
 
     }
--- src/edu/neu/xsz/leetcode/lcof/lcof40/Solution2.java
+++ src/edu/neu/xsz/leetcode/lcof/lcof40/Solution2.java
@@ -1,7 +1,17 @@
 package edu.neu.xsz.leetcode.lcof.lcof40;
 
+import java.util.PriorityQueue;
+
 /**
  * 使用堆
  */
 public class Solution2 {
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(arr.length, (o1, o2) -> o1 - o2);
+        for (int x : arr) pq.offer(x);
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) ans[i] = pq.poll();
+        return ans;
+    }
 }
